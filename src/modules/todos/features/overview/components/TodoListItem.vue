30d7e88c9454c23d6a78f201a302bc59e36cd036
YoranByloosPXL <93255063+YoranByloosPXL@users.noreply.github.com>
2025-03-06 14:21:21 +0100

    Resolved Github comments

--- src/modules/todos/features/overview/components/TodoListItem.vue
+++ src/modules/todos/features/overview/components/TodoListItem.vue
@@ -15,8 +15,8 @@ const props = defineProps<{
 </script>
 
 <template>
-  <li class="p-lg m-md bg-white shadow rounded-lg border border-gray-200 flex items-center justify-between mt-5">
-    <div class="flex items-start gap-3">
+  <li class="p-lg m-md bg-white shadow rounded-lg border border-gray-200 flex items-center justify-between mt-lg">
+    <div class="flex items-start gap-lg">
       <VcCheckbox :checked="props.todo.completed" />
       <div>
         <h1 class="font-semibold text-lg">
@@ -26,7 +26,7 @@ const props = defineProps<{
           {{ props.todo.description }}
         </p>
 
-        <div class="flex items-center text-sm text-gray-400 mt-2">
+        <div class="flex items-center text-sm text-gray-400 mt-sm">
           <span :class="{ 'text-red-500': !props.todo.deadline }">
             {{ props.todo.deadline ? DateFormatUtil.toLongDate(props.todo.deadline) : '' }}
           </span>
